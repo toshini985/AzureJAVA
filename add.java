74c9401861fa9152578945990a50d328f620aa3c
Gottimukkala Toshini <73868843+toshini985@users.noreply.github.com>
2021-09-18 16:57:05 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,5 +1,7 @@
-public class Sum{
-public static void main(String args[]){
+public class add
+{
+public static void main(String args[])
+{
 int n1=100,n2=200,sum;
 sum=n1+n2;
 System.out.println("The sum of numbers is: "+sum);
